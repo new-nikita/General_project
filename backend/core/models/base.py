675cbf6fe142963bae2c032ae095e5d1cebdabcc
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-03-29 18:34:34 +0400

    added base for sqlalchemy models

---
+++ backend/core/models/base.py
@@ -0,0 +1,17 @@
+# from sqlalchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+
+class Base(DeclarativeBase):
+    """Базовый класс для всех моделей."""
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(self) -> str:
+        """
+        Добавление всем классам наследникам название таблиц название класса в нижнем регистре,
+        с добавлением 's' обозначающее множественное число.
+        """
+        return f"{self.__class__.__name__.lower()}s"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
