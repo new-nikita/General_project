b87936dabfe941ea6cd508076d4d0f30af1f1df3
Nikita Popkov <nik.popkov.98@list.ru>
2025-03-30 15:01:41 +0300

    registration_0.1

--- backend/auth/registration_user.py
+++ backend/auth/registration_user.py
@@ -1,15 +1,39 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Response, Depends
 from authx import AuthX, AuthXConfig
+from pydantic import BaseModel
+
 
 router = APIRouter(tags=["Users"])
 
+config = AuthXConfig()
+config.JWT_SECRET_KEY = "SECRET_KEY"
+config.JWT_ACCESS_COOKIE_NAME = "my_access_key"
+config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=config)
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
 
 @router.post('/login')
-def login():
-    return 'login'
+def login(creds: User, response: Response):
+    if creds.username == 'test' and creds.password == 'test':
+        # создается токен id пользователя
+        token = security.create_access_token(uid='12345')
+        # передача куки
+        response.set_cookie(config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+
+    # ошибка которую получит пользователь, ВАЖНО указать код ошибки 401
+    raise HTTPException(status_code=401, detail='Incorrect username or password')
 
 
-@router.get('/protected')
+# Проверяем, зарегистрированный пользователь или нет
+@router.get('/protected', dependencies=[Depends(security.access_token_required)])
 def protected():
-    return 'protected'
+    # пример данных на отправления если пользователь зарегистрирован
+    return {"data": "TOP SECRET"}
 
