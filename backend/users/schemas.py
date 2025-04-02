edd0c3fbe997352b17574a92ed841946feeb84eb
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-04-02 23:30:17 +0400

    added pydantic schemas by User

--- backend/users/schemas.py
+++ backend/users/schemas.py
@@ -1,18 +1,55 @@
+from datetime import datetime
 from typing import Annotated
 from annotated_types import MinLen, MaxLen
-from pydantic import BaseModel, EmailStr, ConfigDict
+
+from pydantic import BaseModel, EmailStr, ConfigDict, Field, constr
+from pydantic.functional_validators import AfterValidator
+from utils.validated import validate_username
 
 
-class CreateUser(BaseModel):
-    username: Annotated[str, MinLen(3), MaxLen(20)]
-    email: EmailStr
-    password: str
-
-
-class UserSchema(BaseModel):
-    model_config = ConfigDict(strict=True)
+class UserBase(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
 
     username: str
-    password: bytes
-    email: EmailStr | str | None = None
-    active: bool = True
+    email: EmailStr
+
+
+class UserCreate(BaseModel):
+    username: constr(min_length=3, max_length=20) = Field(
+        ...,
+        description="Username must be between 3 and 20 characters "
+        "and can only contain letters, numbers, and underscores",
+    )
+    password: str = Field(
+        ...,
+        min_length=8,
+        description="Password must contain at least 8 characters",
+    )
+    email: EmailStr = Field(..., max_length=100)
+
+
+class UserUpdate(BaseModel):
+    username: Annotated[
+        str | None, MinLen(3), MaxLen(20), AfterValidator(validate_username)
+    ] = None
+    password: Annotated[
+        str | None,
+        MinLen(8),
+        Field(description="Password must contain at least 8 characters"),
+    ] = None
+    email: Annotated[EmailStr | None, Field(..., max_length=100)] = None
+
+
+class UserResponse(UserBase):
+    id: int
+    is_active: bool = True
+    created_at: datetime
+
+
+class UserInDB(UserBase):
+    id: int
+    hashed_password: str
+    is_active: bool
+    is_superuser: bool = False
+    created_at: datetime
+    updated_at: datetime | None = None
