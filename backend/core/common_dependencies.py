bb29a5dc35358a0d86e4314766369ea7d540cac5
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-04-03 20:21:24 +0400

    added common dependencies for the entire application

---
+++ backend/core/common_dependencies.py
@@ -0,0 +1,17 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from core.models.db_helper import db_helper
+
+
+async def get_db_session() -> AsyncSession:
+    """
+    Возвращает асинхронную сессию базы данных.
+
+    Использует `db_helper.session_getter` для создания новой сессии SQLAlchemy.
+    Сессия автоматически закрывается после завершения использования.
+
+    Returns:
+        AsyncSession: Асинхронная сессия SQLAlchemy.
+    """
+    async for session in db_helper.session_getter():
+        yield session
