05c28852f865c5d6310155f6bc9c5181f0352b73
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-04-15 19:00:32 +0400

    deleted all Fields by schema

--- backend/users/schemas/profile_schemas.py
+++ backend/users/schemas/profile_schemas.py
@@ -17,19 +17,19 @@ class ProfileCreate(BaseModel):
     Все поля являются необязательными.
     """
 
-    first_name: Optional[str] = Field(None, min_length=2, max_length=50)
-    last_name: Optional[str] = Field(None, min_length=2, max_length=50)
-    middle_name: Optional[str] = Field(None, max_length=50)
+    first_name: Optional[str]
+    last_name: Optional[str]
+    middle_name: Optional[str]
 
-    birth_date: Optional[date] = Field(None)
-    gender: Optional[str] = Field(None, max_length=10)
+    birth_date: Optional[date] | str
+    gender: Optional[str]
 
-    phone_number: Optional[str] = Field(None, max_length=20)
-    country: Optional[str] = Field(None, max_length=50)
-    city: Optional[str] = Field(None, max_length=50)
-    street: Optional[str] = Field(None, max_length=100)
+    phone_number: Optional[str]
+    country: Optional[str]
+    city: Optional[str]
+    street: Optional[str]
 
-    bio: Optional[str] = Field(None)
+    bio: Optional[str]
 
     @field_validator("phone_number")
     def validate_phone_number(cls, value: str | None) -> str | None:
@@ -38,7 +38,7 @@ class ProfileCreate(BaseModel):
         Если номер начинается с '8', преобразуем его в формат '+7'.
         """
         if not value:
-            return
+            return ""
 
         # Удаляем пробелы, дефисы и скобки из номера
         cleaned_value = re.sub(r"[^\d]", "", value)
