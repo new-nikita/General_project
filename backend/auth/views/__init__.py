3c0a234e51b68cac80bfe671df5e4c6c66955a30
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-05-04 18:59:02 +0400

    Изменил вывод side-bar, чтобы на страницах входа или регистрации не было меню со ссылками на данные профиля. И сделал вывод на index.html всех постов, чтобы страница не была пустой

--- backend/auth/views/__init__.py
+++ backend/auth/views/__init__.py
@@ -16,7 +16,8 @@ from auth.authorization import (
 )
 from auth.views.login_view import router as login_router
 from auth.views.register_view import router as register_router
-
+from posts.services import PostService
+from posts.dependencies import get_post_service
 
 logging.basicConfig(
     format=settings.logging.log_format, level=settings.logging.log_level_value
@@ -33,18 +34,22 @@ router.include_router(register_router)
 async def index(
     request: Request,
     current_user: Annotated[User, Depends(get_current_user_from_cookie)],
+    post_service: Annotated[PostService, Depends(get_post_service)],
 ):
     """
     Отображает главную страницу с приветствием.
 
     :param request: Запрос FastAPI.
     :param current_user: Текущий авторизованный пользователь.
+    :param post_service: Сервис для работы с постами.
     :return: HTML-страница с приветствием.
     """
+    posts = await post_service.repository.get_all()
     return settings.templates.template_dir.TemplateResponse(
         "index.html",
         {
             "request": request,
             "current_user": current_user,
+            "posts": posts,
         },
     )
