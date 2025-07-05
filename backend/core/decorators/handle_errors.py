c7abf22c73e3e4fc78aa733b413b945a5c71dfd9
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-07-05 20:40:45 +0400

    добавил интерфейс для обработки ошибок декоратором в TokenRefreshMiddleware. mypy ругался на типы.

--- backend/core/decorators/handle_errors.py
+++ backend/core/decorators/handle_errors.py
@@ -1,18 +1,25 @@
 from functools import wraps
-from typing import ParamSpec, TypeVar, Callable, Awaitable
+from typing import (
+    Awaitable,
+    Callable,
+    ParamSpec,
+    cast,
+)
 
 from fastapi import HTTPException
 from fastapi.responses import Response
 
+from backend.interfaces.error_handler_middleware_protocol import (
+    TokenRefreshErrorsHandler,
+)
+
 P = ParamSpec("P")
-R = TypeVar("R", bound="TokenRefreshMiddleware")
 
 
 def handle_token_errors(
     func: Callable[P, Awaitable[Response]],
 ) -> Callable[P, Awaitable[Response]]:
-    """
-    Декоратор для обработки ошибок аутентификации в middleware.
+    """Декоратор для обработки ошибок аутентификации в middleware.
 
     Этот декоратор оборачивает асинхронную функцию и обрабатывает
     исключения, связанные с аутентификацией. Он перехватывает
@@ -27,16 +34,26 @@ def handle_token_errors(
     """
 
     @wraps(func)
-    async def wrapper(self: R, *args: P.args, **kwargs: P.kwargs) -> Response:
-        """
-        Обертка для обработки ошибок в middleware.
-        :param self: Это текущий объект middleware (TokenRefreshMiddleware).
+    async def wrapper(*args: P.args, **kwargs: P.kwargs) -> Response:
+        """Обертка для обработки ошибок в middleware.
+
+        `self` - это экземпляр TokenRefreshMiddleware, который
+        обязан выполнять интерфейс TokenRefreshErrorsHandler'а
+        с обработками ошибок и возвратом ответов клиенту.
+
+        Функция cast из модуля typing не создает никаких объектов
+         - это исключительно подсказка для статического анализа типов (mypy)
         """
         try:
-            return await func(self, *args, **kwargs)
+            response: Response = await func(*args, **kwargs)
+            return response
         except HTTPException as http_exception:
+            # Важно! на самом деле тут не TokenRefreshErrorsHandler,
+            # а TokenRefreshMiddleware
+            self = cast(TokenRefreshErrorsHandler, args[0])
             return self._handle_auth_error(http_exception)
         except Exception as base_exception:
+            self = cast(TokenRefreshErrorsHandler, args[0])
             return self._handle_unexpected_error(base_exception)
 
     return wrapper
