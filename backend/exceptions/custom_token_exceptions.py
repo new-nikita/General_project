e81e2282cae9e4923734a453773322250fb68e24
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-07-05 21:01:19 +0400

    изменил имя модуля custom_token_exceptions.py на token_exceptions.py, более коротко и проще читаемое. в pre-commit добавил игнорирование длины строк в 79 символов, но указал допустимый максимум 100

--- backend/exceptions/custom_token_exceptions.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException
-
-
-class InvalidTokenError(HTTPException):
-    """Исключение для невалидного токена в запросе."""
-
-    def __init__(self) -> None:
-        super().__init__(status_code=401, detail="Invalid token")
