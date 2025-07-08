ae88ddcd95b2aa38b5efb2bd36aa1d3923e603c1
Дмитрий Афанасьев <wackymc@yandex.ru>
2025-07-08 16:35:33 +0400

    extra code removed

--- backend/auth/__init__.py
+++
@@ -1,9 +0,0 @@
-from .redis_client import AsyncRedisClient
-from .token_cookie_service import TokenCookieService
-from .tokens_service import TokenService
-
-__all__ = (
-    "AsyncRedisClient",
-    "TokenCookieService",
-    "TokenService",
-)
