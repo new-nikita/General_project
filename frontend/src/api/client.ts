2250dbcfdc0785a7b542e5723812140c22b543aa
Nikita Popkov <nik.popkov.98@list.ru>
2026-07-25 18:17:25 +0300

    good_version 0.1.1

--- frontend/src/api/client.ts
+++
@@ -1,9 +0,0 @@
-// frontend/src/api/client.ts
-import axios from "axios"
-
-const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
-  withCredentials: true,
-})
-
-export default api
